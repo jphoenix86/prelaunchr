ab508d4335cd0ca9d9e9bdf17c7af8083e105e12
Phil Phoenix <phillip.matthew.phoenix@gmail.com>
2014-07-22 17:39:11 -0600

    Changed wording

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || 'xxxxxxxx-change-me-please-xxxxxxxx'
+Prelaunchr::Application.config.secret_token = ENV["RAILS_SECRET"] || 'e47507e12c822760848fc2417e8dfd2c8b54eb019c4429795f82e22769aa9ce5ee6671739482c15f7f98bfbffb90e9be74681a1ec2f1fe1d90666f20d6ff721e'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-AdminUser.create!(:email => 'admin@prelaunchr.com', :password => 'changeme', :password_confirmation => 'changeme')
+AdminUser.create!(:email => 'phillip.matthew.phoenix@gmail.com', :password => 'jamesbond1', :password_confirmation => 'jamesbond1')
